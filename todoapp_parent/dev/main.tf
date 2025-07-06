9234bc5f72e91d534ff3b501562977bc10f5469f
Your Name <your-email@example.com>
2025-07-07 00:55:06 +0530

    deleted

--- todoapp_parent/dev/main.tf
+++ todoapp_parent/dev/main.tf
@@ -128,27 +128,6 @@ module "sql_server" {
   secret_password     = "vm-password"
 
 }
-module "sql_serve3r" {
-  depends_on          = [module.resource_group, module.secrets, module.key_vault]
-  source              = "../../modules/azurerm_sql_server"
-  sql_server_name     = "todoapp-3ae-sqlserver"
-  resource_group_name = "TODOAP3P-AE-RG"
-  location            = "Austr3alia East"
-  key_vault_name      = "TODOAPP-AE-KV"
-  secret_username     = "vm-username"
-  secret_password     = "vm-password"
-}
-module "sql_serve1r" {
-  depends_on          = [module.resource_group, module.secrets, module.key_vault]
-  source              = "../../modules/azurerm_sql_server"
-  sql_server_name     = "todoapp-ae-sqlserver"
-  resource_group_name = "TODOA1PP-AE-RG"
-  location            = "Austra1lia East"
-  key_vault_name      = "TODOAPP-AE-KV"
-  secret_username     = "vm-username"
-  secret_password     = "vm-password"
-
-}
 
 module "sql_database" {
   depends_on          = [module.sql_server]
